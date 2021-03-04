2afc0c61715cf0cf5c36874afddac2a655d91065
BYP <vullro@gmail.com>
2021-03-05 05:50:37 +0900

    Application / HelloController / HelloControllerAdd add..

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +13,9 @@ public class HelloController {
         return "hello";
     }
 
+    @GetMapping("/hello/dto")
+    public HelloResponseDto hello(@RequestParam("name") String name, @RequestParam("amount") int amount){
+
+        return new HelloResponseDto(name, amount);
+    }
 }
