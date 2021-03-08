ee7af9007fba964d9b13fabbe09b9ddc219bdd74
BYP <vullro@gmail.com>
2021-03-08 14:47:22 +0900

    Posts add / modify / find methods 추가 BaseTimeEntity 추가

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -25,7 +25,12 @@ public class Posts {
     @Builder
     public Posts(String title, String content, String author) {
         this.title = title;
+        this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
         this.content = content;
     }
 }
