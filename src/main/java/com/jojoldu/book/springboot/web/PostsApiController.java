27a122c6cda896df1eb11455cee5d5b24e791501
BYP <vullro@gmail.com>
2021-03-16 06:45:22 +0900

    google login

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -28,4 +28,9 @@ public class PostsApiController {
     public PostsResponseDto findById(@PathVariable Long id){
         return postsService.findById(id);
     }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        return postsService.delete(id);
+    }
 }
